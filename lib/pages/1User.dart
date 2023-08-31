803008fd435e6eae4cb13dfc0fc66da8584e81ab
el-tiji <manuel_tinjaca@hotmail.com>
2023-08-30 20:22:25 -0500

    Initial Commit

---
+++ lib/pages/1User.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class User1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 110, 173, 255),
+        title: Center(
+          child: Text('Pope Cleaning'),
+        ),
+      ),
+      body: Center(child: Text('contenido del usuario'),)
+    );
+  }
+}
