5a68e04a106f187e9729030d48a57eacf1c844da
el-tiji <manuel_tinjaca@hotmail.com>
2023-09-19 18:55:59 -0500

    cambio

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:pope01/pages/login.dart';
-import 'pages/homePages.dart';
+//import 'pages/homePages.dart';
 
-void main(){ 
-runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {  
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: loginPage(),
     );
   }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,37 +6,97 @@ class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 110, 173, 255),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 50),
-
-            //logo
-            const Icon(
-              Icons.lock,
-              size: 100,
-            ),
-            const SizedBox(height: 50),
-
-            //texto de usuario
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderSide(color: Colors.white),
-                    ),
-                    fillColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 110, 173, 255),
+        title: Center(child: Text('login with SingUp')),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50.0),
+                Text(
+                  'Sing In',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 40.0),
                 ),
-                
-              ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'codigo ejemplo',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 25.0),
+                ),
+//user name----------------------------------------------
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  margin: EdgeInsets.only(top: 150.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'user name',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+//password----------------------------------------------
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'password',
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+//boton Sing In
+                Container(
+                  margin: EdgeInsets.all(40.0),
+                  width: double.infinity,
+                  child: TextButton(
+                    child: Text(
+                      'Sing In',
+                      style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {},
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  ),
+              ],
             ),
-            //texto de contraseña
-          ],
+          ),
         ),
       ),
     );
